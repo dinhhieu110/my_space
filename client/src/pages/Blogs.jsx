1153341b0583ce6a603814eaa12511d8671b6cf8
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-08-17 17:04:34 +0700

    [MS-01] Setup routers and pages

---
+++ client/src/pages/Blogs.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/layout/Navbar";
+import Hero from "../components/common/Hero";
+import BlogList from "../components/common/BlogList";
+
+const Blogs = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <BlogList />
+      Blogs page
+    </>
+  );
+};
+
+export default Blogs;
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Me } from "./Me";
+export { default as Blogs } from "./Blogs";
+export { default as Blog } from "./Blog";
