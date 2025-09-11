67d37cf51683a4cbde35668dc8c62aad763918ca
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-09-11 17:21:42 +0700

    [MS-21] Handle search/filter blogs

--- client/src/components/blogs/Hero.jsx
+++ client/src/components/blogs/Hero.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { useGlobalState } from "../../contexts/AppContext";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 const Hero = () => {
+  const { filter, setFilter } = useGlobalState();
+  const [searchText, setSearchText] = React.useState("");
+
+  const handleSearchBlog = async (e) => {
+    e.preventDefault();
+    setFilter(searchText);
+  };
+
+  const onClear = () => {
+    setFilter("");
+  };
+
+  useEffect(() => {
+    if (!searchText && filter !== "") {
+      onClear();
+    }
+  }, [searchText]);
+
   return (
     <div className="mx-8 sm:mx-16 xl:mx-24">
-      <div className="text-center mt-20 mb-8">
+      <div className="text-center mt-20 mb-8 relative">
         <div className="inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary">
           <p>New AI feature integrated</p>
           <img src={assets.star_icon} className="w-2.5" alt="star icon" />
@@ -16,8 +37,13 @@ const Hero = () => {
           This is my space to share what matters, and to write without filters.
           Whether it's one word or a thousand, my story starts right here.
         </p>
-        <form className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden">
+        <form
+          onSubmit={handleSearchBlog}
+          className="flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden"
+        >
           <input
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
             className="outline-none w-full pl-4"
             type="text"
             placeholder="Search for blog"
